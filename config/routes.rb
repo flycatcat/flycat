cf78596e9f7f2e04c57ba19e9333997815f754f6
Yunxi <a61390642@gmail.com>
2022-05-05 17:37:36 +0800

    feat: 🎸 Re-install tailwind and update 404/500 error pages
    ✅ Closes: #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
-  root 'pages#index'
+  match "/404", to: "application#not_found", via: :all
+  match "/500", to: "application#internal_server_error", via: :all
+  root 'application#not_found'
 end
