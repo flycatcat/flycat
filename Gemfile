5c665d0226369175bc29b981c316cf6cb2a3b388
Yunxi <a61390642@gmail.com>
2022-05-05 16:41:02 +0800

    feat: 🎸 Re-install tailwind and update 404/500 error pages
    ✅ Closes: #9

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ end
 group :development do
   gem 'foreman', '~> 0.87.2'
   gem 'web-console', '>= 4.1.0'
-  gem 'rack-mini-profiler', '~> 2.0'
+  # gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   gem 'spring'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  root 'pages#index'
 end
