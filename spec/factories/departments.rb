61e865bcc050da12ac270f80019a87d316768240
Yunxi <a61390642@gmail.com>
2022-05-25 16:19:32 +0800

    feat: 🎸 No.43 新增請假證明上傳功能
    No.43 新增請假證明上傳功能
    
    ✅ Closes: #43

--- spec/factories/departments.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :department do
-    department_name { 'MyString' }
-  end
-end
--- spec/views/companies/update.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'companies/update.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
