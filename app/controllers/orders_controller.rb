f2d3d892e7b01ed936f2e0877c991caf920956b0
Daisy <ysiad926@gmail.com>
2022-05-31 00:28:14 +0800

    docs(orders): controller
    skip_before_action 改寫

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-  skip_before_action :verify_authenticity_token, except: :create
+  skip_before_action :verify_authenticity_token, only: %i[new confirm success]
 
   def new
     @order = Order.new
