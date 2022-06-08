02911c19759f3401742bab020b0e1fe357d2e015
Daisy <ysiad926@gmail.com>
2022-06-08 23:08:32 +0800

    解衝突

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ Time::DATE_FORMATS.merge!(default: '%Y/%m/%d %H:%M:%S', ymd: '%Y/%m/%d')
 module Flycat
   class Application < Rails::Application
     config.load_defaults 6.1
+<<<<<<< HEAD
     config.i18n.available_locales = %i[en zh-TW]
+=======
+    config.exceptions_app = routes
+    config.i18n.available_locales = %i[en zh-TW ja]
+>>>>>>> 476d395 (docs(錯誤訊ｐ息): 註冊)
     config.i18n.default_locale = :'zh-TW'
     config.hosts << 'flycat.works'
     config.hosts << "flycat0607.herokuapp.com"
@@ -18,7 +23,7 @@ module Flycat
     config.action_controller.forgery_protection_origin_check = false
     config.time_zone = 'Taipei'
     config.active_record.default_timezone = :local
-    config.i18n.available_locales = %i[en zh-TW]
+    config.i18n.available_locales = %i[en zh-TW ja]
     config.i18n.default_locale = :'zh-TW'
   end
 end
