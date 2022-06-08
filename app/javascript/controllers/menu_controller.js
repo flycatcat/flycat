dd28c0d26621199959107796f49a3b10385ccbdd
Daisy <ysiad926@gmail.com>
2022-06-08 22:59:48 +0800

    docs(解衝突): merge

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Controller } from "stimulus";
 
 export default class extends Controller {
@@ -7,3 +8,14 @@ export default class extends Controller {
     this.toggleableTarget.classList.toggle("hidden");
   }
 }
+=======
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["toggleable"]
+
+  toggle(){
+      this.toggleableTarget.classList.toggle('hidden')
+  }
+}
+>>>>>>> ee0b706 (docs(i18n): dropdown menu)
